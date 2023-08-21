0719059faa0c175f6b823a7c1d509cb8c39c6775
Pablo Muñoz <pablommartinezdev@gmail.com>
2023-08-21 11:55:23 +0200

    Add project files.

---
+++ FoodiePal/Shared/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodiePal.Shared.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName1 { get; set; }
+        public string LastName2 { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public ICollection<Address> Addresses { get; set; } = new List<Address>();
+        public int RolId { get; set; }
+        public Rol Rol{ get; set; }
+    }
+}
